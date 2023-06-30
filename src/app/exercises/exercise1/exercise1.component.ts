85b0b48d8e0b32dad6a6afa116f0fe6333ce1852
kochutreasia <114157670+kochutreasia@users.noreply.github.com>
2023-06-30 11:42:44 +0530

    3 files modified

--- src/app/exercises/exercise1/exercise1.component.ts
+++ src/app/exercises/exercise1/exercise1.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  AbstractControl,
+  FormControl,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-exercise1',
   templateUrl: './exercise1.component.html',
-  styleUrls: ['./exercise1.component.css']
+  styleUrls: ['./exercise1.component.css'],
 })
-export class Exercise1Component {
+export class Exercise1Component implements OnInit {
+  form: FormGroup = this.formBuilder.group({});
 
+  constructor(private formBuilder: FormBuilder) {}
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      username: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(4),
+          Validators.maxLength(10),
+          this.passwordValidator,
+        ],
+      ],
+    });
+  }
+  get f(): { [key: string]: AbstractControl } {
+    return this.form.controls;
+  }
+  passwordValidator(control: FormControl) {
+    if (!/\W/.test(control.value)) {
+      return { specialChars: true };
+    }
+    return null;
+  }
+
+  onSubmit() {}
 }
+
--- src/app/exercises/exercise1/exercise1.module.ts
+++ src/app/exercises/exercise1/exercise1.module.ts
@@ -2,19 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Exercise1Component } from './exercise1.component';
 import {RouterModule} from "@angular/router";
-
-
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    Exercise1Component
-  ],
+  declarations: [Exercise1Component],
   imports: [
     CommonModule,
-    RouterModule.forChild([{
-      path: '',
-      component: Exercise1Component
-    }])
-  ]
+    RouterModule.forChild([
+      {
+        path: '',
+        component: Exercise1Component,
+      },
+    ]),
+    ReactiveFormsModule,
+  ],
 })
-export class Exercise1Module { }
+export class Exercise1Module {}
